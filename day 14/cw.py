a2a6b7f83dedaf475914f0f466f7444ef72753ca
gabrieli2014 <khelaia>
2025-05-27 22:14:19 +0400

    second commit

---
+++ day 14/cw.py
@@ -0,0 +1,13 @@
+age=input ('რამედენი წლის ხარ?:')
+experience=input('რამდენი წლის გამოცდილება გაქვს:')
+heigh=input('რამდენი მეტრო სიმაღლის ხარ?:')
+
+ishired= (1.75<=heigh)  (18<=age) (2<=experience) 
+print("you are hired")
+
+
+
+
+
+
+
---
+++ day 14/hw.py
@@ -0,0 +1,24 @@
+# > მეტობის ნიშანი < ნაკლებობის ნიშანი == ტოლობის ნიშანი <= მეტობის და ტოლობის ნიშანი <= ნაკლებობის და ტოლობის ნიშანი
+
+# and, or, not
+
+
+ishired=10>11 and 11==11 or 20>15 
+
+print('you are hired: '+  str(ishired))
+
+
+
+print((True and not False and (False or True) and True or not False and False and not True and (True or False) or (True and False)))
+ 
+  #true and not false =true
+  #false or true = true 
+  #true and true = true
+  #true and true = true
+  #false and false = false
+  #false and not true =false 
+  #true or false = true 
+  #false and true = false 
+  #true and not false = true
+  #true and false = false
+  #true or false = true
