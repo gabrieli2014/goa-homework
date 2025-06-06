ac442b474568d09327592b383460bf50d59f700a
gabrieli2014 <khelaia>
2025-06-06 22:13:33 +0400

    second commit

---
+++ day 16/homework/hw.py
@@ -0,0 +1,18 @@
+for i in range(1,20):
+    print(i)
+
+    i =+ 1
+
+for i in range(1,20,2):
+    print(i)
+    i =+ 1
+#for ციკლი გამოიყენება როცა გამეორებების რაოდენობა ცნობილია, while ციკლი – როცა პირობით წყდება.
+
+#forever loop ნიშნავს ტერმინალში რიცხვს იქამდე გაიმოტანს სან ჩვენ არ შევაჩერებთ
+
+
+counter = 15  
+
+while counter <= 20:  
+    print(counter)     
+    counter=counter + 1
