11566eaa426d62a10ed3be08dc5d4250c9b749d0
gabrieli2014 <khelaia>
2025-12-28 12:43:20 +0400

    second-commit

---
+++ day 44/classwork2/cw.js
@@ -0,0 +1,13 @@
+console.log('look')
+
+
+// let ცვლადისმნიშვნელობა შეიძლება შეიცვალოს
+let age = 10;
+age = 11; 
+
+// const ცვლადის მნიშვნელობა შეცვლა აღარ შეიძლება
+const name = "გაბრიელი";
+
+
+let score = 5;        
+const school = "N1"; 
