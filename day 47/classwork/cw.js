873a6ed9cd2eb2c5ed95c2cd23b4e07749d082f6
gabrieli2014 <khelaia>
2026-01-17 18:14:45 +0400

    second-commit

---
+++ day 47/classwork/cw.js
@@ -0,0 +1,32 @@
+
+let num1 = Number(prompt("შეიყვანე პირველი რიცხვი"));
+
+
+let num2 = Number(prompt("შეიყვანე მეორე რიცხვი"));
+
+
+console.log("შეკრება:", num1 + num2);
+
+
+console.log("გამოკლება:", num1 - num2);
+
+
+console.log("გამრავლება:", num1 * num2);
+
+
+console.log("გაყოფა:", num1 / num2);
+
+
+console.log("ნაშთი:", num1 % num2);
+
+
+
+let age = 10;
+age = 11; 
+console.log(age);
+
+
+const name = "Gabriel";
+
+console.log(name);
+
---
+++ day 47/homework/hw.js
@@ -0,0 +1,22 @@
+// 0) prompt ფუნქცია აჩვენებს ფანჯარას, სადაც მომხმარებელს შეუძლია ტექსტის შეყვანა.
+// შეყვანილი მნიშვნელობა ინახება ცვლადში როგორც string (ტექსტი).
+
+
+let name = prompt("შეიყვანე შენი სახელი");
+alert(name);
+
+
+let num1 = prompt("შეიყვანე პირველი რიცხვი");
+let num2 = prompt("შეიყვანე მეორე რიცხვი");
+
+
+let sum = Number(num1) + Number(num2);
+console.log(sum);
+
+
+let age = prompt("შეიყვანე შენი ასაკი");
+alert("შენი ასაკია: " + age);
+
+
+let color = prompt("შეიყვანე შენი საყვარელი ფერი");
+console.log(color);
