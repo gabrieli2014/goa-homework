b549697b818bbee3a19b7da1399f0bca6cdc76c7
gabrieli2014 <khelaia>
2026-03-28 10:07:10 +0400

    second-commit

---
+++ day 56/classwork/cw.js
@@ -0,0 +1,9 @@
+
+  let texts = document.getElementsByClassName("text");
+  texts[0].innerHTML = "ეს ტექსტი შეცვლილია class-ით";
+
+
+  let paragraphs = document.getElementsByTagName("p");
+  paragraphs[1].innerHTML = "ეს ტექსტი შეცვლილია tag-ით";
+
+  document.body.innerHTML = "<h1>გამარჯობა, მე ვარ jsით შეცვლილი საიტი</h1>";
---
+++ day 56/homework/hw.js
@@ -0,0 +1,19 @@
+function changeText() {
+    document.getElementById("text").innerText = "ტექსტი შეიცვალა!";
+  }
+
+  function changeColor() {
+    document.getElementById("title").style.color = "red";
+  }
+
+  function changeBackground() {
+    document.getElementById("box").style.backgroundColor = "blue";
+  }
+
+  function changeImage() {
+    document.getElementById("image").src = "cat.png";
+  }
+
+  function increaseSize() {
+    document.getElementById("sizeText").style.fontSize = "30px";
+  }
