1e6e8feda24c9762d7d5e1866ba6b3411ca2436f
gabrieli2014 <khelaia>
2026-04-04 15:32:55 +0400

    second-commit

---
+++ day 57/homework/hw.js
@@ -0,0 +1,12 @@
+function add(value) {
+    document.getElementById("display").value += value;
+}
+
+function calculate() {
+    let result = document.getElementById("display").value;
+    document.getElementById("display").value = eval(result);
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
